405134eef9c1c1e51564e0cd025dabd7754636d3
Rohan Panda <rohan.panda1@gmail.com>
2019-09-18 20:34:57 +0530

    Fix login

--- index.js
+++ index.js
@@ -1,87 +1,87 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bluebird = require("bluebird");
-const bodyParser = require("body-parser");
-const User = require("./schemas/user");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+/* eslint-disable no-console */
+const express = require('express');
+const mongoose = require('mongoose');
+const bluebird = require('bluebird');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('./schemas/user');
 
 const app = express();
 const port = 3000;
-const url = "mongodb://localhost:27017/mydb";
+const url = 'mongodb://localhost:27017/mydb';
 
 mongoose.Promise = bluebird;
 mongoose.connect(url, {
   autoReconnect: true,
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
-db = mongoose.connection;
+const db = mongoose.connection;
 
-db.on("connecting", () => console.log("Connecting to MongoDB."));
+db.on('connecting', () => console.log('Connecting to MongoDB.'));
 
-db.on("error", error => {
+db.on('error', (error) => {
   console.error(`Error in MongoDB connection: ${error}`);
   mongoose.disconnect();
 });
 
-db.on("connected", () => {
-  console.log("Connected to MongoDB!");
+db.on('connected', () => {
+  console.log('Connected to MongoDB!');
 });
 
-app.listen(port, () => console.log("App started."));
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.listen(port, () => console.log('App started.'));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post("/create", (req, res) => {
-  if (req.body.email && req.body.username && req.body.password) {
-    const userData = {
-      username: req.body.username,
-      password: req.body.password
-    };
-    //use schema.create to insert data into the db
-    User.create(userData, function(err, user) {
-      if (err) {
-        return res.send({ error: true, msg: err });
-      } else {
-        const token = jwt.sign(userData, "supersecret", {
-          expiresIn: 120
+app.post('/create', (req, res) => {
+  const { body: { email, name, password } = {} } = req;
+  if (name && email && password) {
+    bcrypt.hash(password, 10, (e, hash) => {
+      const userData = {
+        name,
+        email,
+        password: hash,
+      };
+      User.create(userData, (err) => {
+        if (err) {
+          return res.send({ error: true, msg: err.errmsg });
+        }
+        const token = jwt.sign(userData, 'supersecret', {
+          expiresIn: 120,
         });
         return res.send({ success: true, token });
-      }
+      });
     });
   }
 });
 
-app.post("/login", (req, res) => {
-  const { body: { username, password } = {} } = req;
-  if (username && password) {
-    User.find({ username }, function(err, user) {
+app.post('/login', (req, res) => {
+  const { body: { email, password } = {} } = req;
+  if (email && password) {
+    User.find({ email }, (err, user) => {
       if (err) {
         return res.send({ error: true, msg: err });
-      } else {
-        const [userDetails] = user;
-        if (!userDetails) {
-          return res.send({ success: false, msg: "Username not found" });
-        }
-        const { password: hash } = userDetails;
-        return bcrypt
-          .compare(password, hash)
-          .then(isValid => {
-            if (isValid) {
-              const userData = { username, password };
-              const token = jwt.sign(userData, "supersecret", {
-                expiresIn: 120
-              });
-              return res.send({ success: true, token });
-            } else {
-              return res.send({ success: false, msg: "Invalid Credentials" });
-            }
-          })
-          .catch(err => res.send({ error: true, msg: err }));
       }
+      const [userDetails] = user;
+      if (!userDetails) {
+        return res.send({ success: false, msg: 'email not found' });
+      }
+      const { password: hash } = userDetails;
+      return bcrypt
+        .compare(password, hash)
+        .then((isValid) => {
+          if (isValid) {
+            const userData = { email, password };
+            const token = jwt.sign(userData, 'supersecret', {
+              expiresIn: 120,
+            });
+            return res.send({ success: true, token });
+          }
+          return res.send({ success: false, msg: 'Invalid Credentials' });
+        })
+        .catch((error) => res.send({ error: true, msg: error }));
     });
   }
 });
--- schemas/user.js
+++ schemas/user.js
@@ -1,35 +1,22 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
-    // unique: true,
+    unique: true,
     required: true,
-    trim: true
+    trim: true,
   },
-  username: {
+  name: {
     type: String,
-    // unique: true,
     required: true,
-    trim: true
+    trim: true,
   },
   password: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
 
-//hashing a password before saving it to the database
-UserSchema.pre("save", function(next) {
-  const user = this;
-  bcrypt.hash(user.password, 10, function(err, hash) {
-    if (err) {
-      return next(err);
-    }
-    user.password = hash;
-    next();
-  });
-});
-
-const User = mongoose.model("user", UserSchema);
+const User = mongoose.model('user', UserSchema);
 module.exports = User;
